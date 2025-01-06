0a78917747da353cd2d56db08cebbe5ce1374476
Rasreal <ersultan.t16@gmail.com>
2025-01-07 03:08:43 +0500

    feat: sort by location added

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,8 @@ import Places from './Places.jsx';
 import {useEffect, useState} from "react";
 import {Error} from "./Error.jsx";
 import {sortPlacesByDistance} from "../loc.js";
+import {fetchAvailablePlaces} from "../http.js";
+
 
 export default function AvailablePlaces({onSelectPlace}) {
 
@@ -11,12 +13,11 @@ export default function AvailablePlaces({onSelectPlace}) {
         async function fetchPlaces() {
 
             try {
-                const response = await fetch('http://localhost:3000/places');
-                const resData = await response.json();
+                const places = await fetchAvailablePlaces();
 
                 navigator.geolocation.getCurrentPosition((pos) => {
                     const sortedPlaces = sortPlacesByDistance(
-                        resData.places,
+                        places,
                         pos.coords.latitude,
                         pos.coords.longitude
                     );
