3eab126292e97ab2618523bc106db7bbc91a01d0
Rasreal <ersultan.t16@gmail.com>
2025-01-11 23:23:20 +0500

    feat: delete logic added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useCallback } from 'react';
+import {useRef, useState, useCallback} from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
@@ -6,81 +6,110 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 import {updateUserPlaces} from "./http.js";
+import {Error} from "./components/Error.jsx";
 
 function App() {
-  const selectedPlace = useRef();
+    const selectedPlace = useRef();
 
-  const [userPlaces, setUserPlaces] = useState([]);
+    const [userPlaces, setUserPlaces] = useState([]);
 
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  function handleStartRemovePlace(place) {
-    setModalIsOpen(true);
-    selectedPlace.current = place;
-  }
+    const [errorUpdPlaces, setErrorUpdPlaces] = useState(null);
 
-  function handleStopRemovePlace() {
-    setModalIsOpen(false);
-  }
-
-  async function handleSelectPlace(selectedPlace) {
-    setUserPlaces((prevPickedPlaces) => {
-      if (!prevPickedPlaces) {
-        prevPickedPlaces = [];
-      }
-      if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
-        return prevPickedPlaces;
-      }
-      return [selectedPlace, ...prevPickedPlaces];
-    });
-
-    try {
-      await updateUserPlaces([selectedPlace, ...userPlaces]);
-
-      console.log("success");
-    } catch (err) {
-      throw err;
+    function handleStartRemovePlace(place) {
+        setModalIsOpen(true);
+        selectedPlace.current = place;
     }
 
-  }
+    function handleStopRemovePlace() {
+        setModalIsOpen(false);
+    }
 
-  const handleRemovePlace = useCallback(async function handleRemovePlace() {
-    setUserPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
+    async function handleSelectPlace(selectedPlace) {
+        setUserPlaces((prevPickedPlaces) => {
+            if(!prevPickedPlaces) {
+                prevPickedPlaces = [];
+            }
+            if(prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
+                return prevPickedPlaces;
+            }
+            return [selectedPlace, ...prevPickedPlaces];
+        });
+
+        try {
+            await updateUserPlaces([selectedPlace, ...userPlaces]);
+
+            console.log("success");
+        } catch (err) {
+            setUserPlaces(userPlaces);
+            setErrorUpdPlaces(
+                {
+                    message: err.message || "Qate bolyp qaldy",
+                }
+            );
+
+        }
+
+    }
+
+    const handleRemovePlace = useCallback(async function handleRemovePlace() {
+        setUserPlaces((prevPickedPlaces) =>
+            prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
+        );
+
+        try{
+            await updateUserPlaces(userPlaces.filter((place) => place.id !== selectedPlace.current.id));
+        } catch(err) {
+            setUserPlaces(userPlaces);
+            setErrorUpdPlaces(
+                {
+                    message: err.message || "Zhoyilmady, qate bolyp qaldy",
+                }
+            );
+        }
+
+        setModalIsOpen(false);
+    }, []);
+
+
+    function handleError() {
+        setErrorUpdPlaces(null);
+    }
+
+    return (
+        <>
+            <Modal open={errorUpdPlaces} onClose={handleError}>
+                {errorUpdPlaces &&
+                    <Error title="Qate shyqty" message={errorUpdPlaces.message} onClose={handleError}/>}
+            </Modal>
+            <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+                <DeleteConfirmation
+                    onCancel={handleStopRemovePlace}
+                    onConfirm={handleRemovePlace}
+                />
+            </Modal>
+
+            <header>
+                <img src={logoImg} alt="Stylized globe"/>
+                <h1>PlacePicker</h1>
+                <p>
+                    Create your personal collection of places you would like to visit or
+                    you have visited.
+                </p>
+            </header>
+            <main>
+                <Places
+                    title="I'd like to visit ..."
+                    fallbackText="Select the places you would like to visit below."
+                    places={userPlaces}
+                    onSelectPlace={handleStartRemovePlace}
+                />
+
+                <AvailablePlaces onSelectPlace={handleSelectPlace}/>
+            </main>
+        </>
     );
-
-    setModalIsOpen(false);
-  }, []);
-
-  return (
-    <>
-      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
-        <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
-        />
-      </Modal>
-
-      <header>
-        <img src={logoImg} alt="Stylized globe" />
-        <h1>PlacePicker</h1>
-        <p>
-          Create your personal collection of places you would like to visit or
-          you have visited.
-        </p>
-      </header>
-      <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText="Select the places you would like to visit below."
-          places={userPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
-
-        <AvailablePlaces onSelectPlace={handleSelectPlace} />
-      </main>
-    </>
-  );
 }
 
 export default App;
